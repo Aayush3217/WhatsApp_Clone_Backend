2a7a14b0d7ca37b6431de831b5fb0ca287e28e83
Aayush pal <Aayushpal3217@gmail.com>
2026-06-25 13:26:04 +0530

    Fix Error

--- services/emailService.js
+++ services/emailService.js
@@ -2,13 +2,24 @@ const nodemailer = require('nodemailer')
 const dotenv = require('dotenv')
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth:{
+//         user : process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// }); 
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-        user : process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-}); 
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+});
 
 transporter.verify((error, success) => {
     if(error){
