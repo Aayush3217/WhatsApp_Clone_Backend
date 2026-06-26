d712fda7b1553a519c1b45997a006f3942395c54
Aayush pal <Aayushpal3217@gmail.com>
2026-06-26 10:36:51 +0530

    Fix bugs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,55 @@
-const { response } = require('express');
-const jwt = require('jsonwebtoken')
+// const { response } = require('express');
+// const jwt = require('jsonwebtoken')
 
+
+// const authMiddleware = (req, res, next) => {
+//     const authToken = req.cookies?.auth_token;
+
+//     if(!authToken){
+//         return response(res, 401, 'authorization token missing. please provide token');
+//     }
+
+//     try{
+//         const decode = jwt.verify(authToken, process.env.JWT_SECRET);
+//         req.user = decode;
+//         next();
+//     }catch(error){
+//         console.log(error);
+//         return response(res, 401, 'Invalid or expired token');
+//     }
+// }
+
+
+// module.exports = authMiddleware;
+
+
+
+const jwt = require("jsonwebtoken");
+const response = require("../utils/responseHandler");
 
 const authMiddleware = (req, res, next) => {
     const authToken = req.cookies?.auth_token;
 
-    if(!authToken){
-        return response(res, 401, 'authorization token missing. please provide token');
+    if (!authToken) {
+        return response(
+            res,
+            401,
+            "Authorization token missing. Please provide token."
+        );
     }
 
-    try{
+    try {
         const decode = jwt.verify(authToken, process.env.JWT_SECRET);
         req.user = decode;
         next();
-    }catch(error){
-        console.log(error);
-        return response(res, 401, 'Invalid or expired token');
+    } catch (error) {
+        console.error(error);
+        return response(
+            res,
+            401,
+            "Invalid or expired token"
+        );
     }
-}
-
+};
 
 module.exports = authMiddleware;
